fcf462668949dc388d2eb71aa336d2559ed3da93
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-24 17:59:59 +0100

    feat[axios/post-file]

--- src/pages/ListaDeiPost.jsx
+++ src/pages/ListaDeiPost.jsx
@@ -1,6 +1,12 @@
+
+// importiamo il componente dei post
+import PostFile from "../components/PostsFile.jsx";
+
 export default function ListaDeiPost() {
     return (
-        <h1>LISTA DEI POST</h1>
-    
+        <>
+            <h1>LISTA DEI POST</h1>
+            <PostFile />
+        </>
     );
 }
